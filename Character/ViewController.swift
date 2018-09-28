c329dd031ea7e968a64b2a8e08ea48681fb0966b
20161104584 <llll@admindeimac-71.local>
2018-09-28 16:46:08 +0800

    Initial Commit

---
+++ Character/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Character
+//
+//  Created by llll on 2018/9/28.
+//  Copyright © 2018年 llll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CharacterTests/CharacterTests.swift
@@ -0,0 +1,34 @@
+//
+//  CharacterTests.swift
+//  CharacterTests
+//
+//  Created by llll on 2018/9/28.
+//  Copyright © 2018年 llll. All rights reserved.
+//
+
+import XCTest
+@testable import Character
+
+class CharacterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
